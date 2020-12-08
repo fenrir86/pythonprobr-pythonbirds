d5113f1816a9ec826aced99ae25f3f8c259783e2
fenrir86 <fenrir.emilio@gmail.com>
2020-12-07 22:41:55 -0300

    Creat the metod cumprimentar metodo alterado

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,11 +2,16 @@
 
 class Pessoa:
 
+    def __init__(self, nome = None):
+        self.none = None
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
     p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+   # print(Pessoa.cumprimentar(p))
+   # print(id(p))
+   # print(p.cumprimentar())
+    p.nome = 'Emilio'
+    print(p.nome)
