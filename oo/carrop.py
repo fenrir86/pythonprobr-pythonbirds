4af02215cffbd0c341dc82169813d90e6dac6e62
fenrir86 <fenrir.emilio@gmail.com>
2020-12-16 09:53:48 -0300

    Classe Carro

--- oo/carrop.py
+++ oo/carrop.py
@@ -59,7 +59,43 @@
     >>> carro.frear()
     >>> carro.calcular_velocidade()
     0
+    >>> carro.calcular_direcao()
+    'Norte'
+    >>> carro.girar_a_direita()
+    >>> carro.calcular_direcao()
+    'Leste'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    'Norte'
+    >>> carro.girar_a_esquerda()
+    >>> carro.calcular_direcao()
+    'Oeste'
 """
+
+class Carro():
+
+
+
+    def __init__(self,direcao,motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+    def frear(self):
+        self.motor.frear()
+    def calcular_direcao(self):
+        return self.direcao.valor
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
+
+
 class Motor:
 
     def __init__(self, velocidade=0):
@@ -87,16 +123,6 @@ class Direcao:
         self.cont -= 1
         self.valor = self.posicao[self.cont]
 
-class Carro:
-    direcao = Direcao()
-    motor = Motor()
-
-    def __init__(self):
-        motor = self.motor.velocidade
-        self.direcao = direcao
-
-    def calcular_velocidade(self):
-        return motor
 
 
 
